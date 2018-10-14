4ee8cf93794712b24c74eba30a172aca4557be3d
Gustavo Henrique Neves Ferreira <gustavoferreira0701@hotmail.com>
2018-10-13 21:12:54 -0300

    Basic Subject

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,26 @@
-import { Observable, from, fromEvent } from "rxjs";
+import { Observable, from, fromEvent, Subject } from "rxjs";
 import { share } from 'rxjs/operators';
 
-var observable = fromEvent(document, 'mousemove');
+var subject = new Subject();
 
-setTimeout(() => {
-    var subscription = observable.subscribe(
-        (x: any) => addItem(x)
-    )
-}, 2000);
+subject.subscribe(
+    data => addItem('Observer 1: ' + data),
+    err => addItem(err),
+    () => addItem('Observer 1 completed!')
+)
+
+subject.next('The first thing has been sent!');
+
+var observer2 = subject.subscribe(
+    data => addItem('Observer 2: ' + data)
+)
+
+subject.next('The second thing has been sent!');
+subject.next('A third thing has been sent!');
+
+observer2.unsubscribe()
+
+subject.next('A final thing has been sent!');
 
 function addItem(val: any) {
     var node = document.createElement("li");
